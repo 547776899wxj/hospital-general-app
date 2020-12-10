0084bb839c1a70818d32f596d6d805d809152931
wxj <547776899@qq.com>
2020-12-10 11:46:33 +0800

    第一次提交

---
+++ pages/index/index.vue
@@ -0,0 +1,157 @@
+<template>
+	<view class="content">
+		<view class="popup">
+			<view class="popup-header">
+				设置{{test}}
+			</view>
+			<view>
+				<form >
+					<view class="uni-form-item " >
+						<view class="popup-title" >ip：</view>
+						<input class="uni-input" name="ip" v-model="ip" placeholder="如:(http://192.168.0.190:8081)" />
+					</view>
+					<view class="uni-form-item " >
+						<view class="popup-title" >路径：</view>
+						<input class="uni-input" name="path" v-model="path"  placeholder="如:(skin/skin)" />
+					</view>
+					<view class="uni-form-item " >
+						<view class="popup-title" >高度：</view>
+						<input class="uni-input" name="height"  v-model="height" placeholder="如:(1080)" />
+					</view>
+					<view class="uni-form-item form-item-bottom">
+						<button class="popup-btn"   @click="clear" >清除</button>
+						<button class="popup-btn"  @click="formSubmit" >确定</button>
+					</view>
+				</form>
+			</view>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				ip:'',
+				path:'',
+				height:'1080',
+				test:'',
+			}
+		},
+		onLoad(e) {
+			this.ip = uni.getStorageSync('ip') || '';
+			this.path = uni.getStorageSync('path') || '';
+			this.height = uni.getStorageSync('height') || '';
+			let webView = e.webView||false;
+			if(this.ip && this.path && !webView){
+				uni.navigateTo({
+					url: 'webn',
+				});
+			}
+		},
+		methods: {
+			//清除
+			clear(){
+				this.ip = '';
+				this.path = '';
+				this.height = '';
+			},
+			//确定
+			formSubmit(e) {
+				if(this.ip=='' || this.path==''){
+					uni.showToast({
+						title: 'ip或路径不能为空',
+						icon:'none'
+					});
+					return
+				}
+				uni.setStorageSync('ip',this.ip);
+				uni.setStorageSync('path',this.path);
+				uni.setStorageSync('height',this.height);
+				uni.redirectTo({
+					url: 'webn',
+				})
+            },
+			navTo(url){
+				console.log(url);
+				uni.redirectTo({
+					url: url,
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+	.image{
+		position: absolute;
+		top: 0;
+	}
+	.content{
+		background-color: #f9f9f9;
+		height: 100%;
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		z-index: -1;
+	}
+	page {
+		height: 100%;
+	}
+	.popup{
+		background-color: #fff;
+		width: 600rpx;
+		font-size: 50rpx;
+		z-index: 1;
+		max-height: 85%;
+		overflow: auto;
+		box-shadow: 0 5px 7px 0 rgb(0 0 0 / 12%)
+	}
+	.popup-header{
+		background-color: rgb(68,114,196);
+		text-align: center;
+		padding: 20rpx 0 ;
+	}
+	.popup-btn {
+		font-size: 30rpx;
+		color: #fff;
+		padding-left: 40rpx;
+		padding-right: 40rpx;
+		background-color: rgb(68, 114, 196);
+		margin-left: 40rpx;
+		margin-right: 40rpx;
+	}
+	.uni-form-item {
+		display: flex;
+		align-items: center;
+		padding: 40rpx 40rpx 0 40rpx;
+		justify-content: center;
+	}
+	.popup-title {
+		font-size: 30rpx;
+		width: 90rpx;
+		text-align: right;
+	}
+	.uni-input {
+		font-size: 25rpx;
+		border: 1px solid;
+		padding: 20rpx 30rpx;
+	}
+	.uni-form-item.form-item-bottom{
+		padding-bottom: 40rpx;
+	}
+	.chooseBtn{
+		font-size: 30rpx;
+		width: 438px;
+	}
+	.radio-group{
+		width: 341px;
+		display: flex;
+	}
+	.uni-list-cell{
+		display: flex;
+		align-items: center;
+	}
+</style>
---
+++ pages/index/web.vue
@@ -0,0 +1,91 @@
+<template>
+	<view >
+		<view class="uni-form-item form-item-bottom" :style="zIndex">
+			<button class="popup-btn" @click="navTo">路径错误点击返回</button>
+		</view>
+		<!-- <web-view class="content" src="https://www.baidu.com/?tn=48021271_22_hao_pg" @message="handleMessage" ></web-view> -->
+		<web-view :src="url" @message="handleMessage" ></web-view>
+	</view>
+</template>
+
+<script>
+	// #ifdef APP-PLUS
+		var FvvUniTTS = uni.requireNativePlugin('Fvv-UniTTS');
+	// #endif
+	export default {
+		data() {
+			return {
+				url:'',
+				zIndex:{
+					'z-index':1000
+				},
+			}
+		},
+		onLoad() {
+			// #ifdef H5
+				this.zIndex = {'z-index':0};
+			// #endif
+			this.url = uni.getStorageSync('ip')+'/#/pages/'+uni.getStorageSync('path');
+		},
+		methods: {
+			navTo(){
+				console.log(123);
+				uni.navigateTo({
+					url: 'index?webView=true',
+				});
+			},
+			handleMessage(res) {
+				// 获取网页的参数
+				let data = res.detail.data[0];
+				if(data.status){
+					this.zIndex = 0;
+				}
+				if(data.reload){
+					// #ifdef APP-PLUS
+					var currentWebview = this.$mp.page.$getAppWebview() //获取当前页面的webview对象
+					var wv = currentWebview.children()[0]
+					wv.reload();
+					// #endif
+					return
+				}
+				console.log(data.text);
+				// #ifdef APP-PLUS
+					if(data.text){
+						FvvUniTTS.init((callback) => {
+							FvvUniTTS.speak({
+								text:data.text
+							});
+						});
+					}
+				// #endif
+			},
+		}
+	}
+</script>
+
+<style>
+	page{
+		height: 100%;
+	}
+	.content{
+		height: 1920px;
+	}
+	.uni-form-item {
+		display: flex;
+		align-items: center;
+		padding: 40rpx 40rpx 0 40rpx;
+		justify-content: center;
+		position: fixed;
+		top: 0;
+		
+	}
+	.popup-btn {
+		font-size: 20rpx;
+		color: #fff;
+		padding-left: 40rpx;
+		padding-right: 40rpx;
+		background-color: rgb(68, 114, 196);
+		margin-left: 40rpx;
+		margin-right: 40rpx;
+	}
+</style>
